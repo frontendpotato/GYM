fc72cd592b25ec2515c2f7d1c48305f2afb5419b
brahim <brahim.cheriet2004@gmail.com>
2025-02-21 18:03:39 +0100

    second commit

--- app/src/main/java/com/example/gym/Homescreen.kt
+++ app/src/main/java/com/example/gym/Homescreen.kt
@@ -5,16 +5,69 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+//class Homescreen : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_homescreen)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//        // Load the default fragment
+//        loadFragment(ExercisesFragment())
+//
+//        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+//        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.navigation_exercises -> loadFragment(ExercisesFragment())
+//                R.id.navigation_diet -> loadFragment(DietFragment())
+//                R.id.navigation_gym -> loadFragment(GymFragment())
+//                R.id.navigation_profile -> loadFragment(ProfileFragment())
+//                else -> false
+//            }
+//            true}
+//    }
+//
+//    private fun loadFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .commit()}
+//}
+
+
+
+
 
 class Homescreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_homescreen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Load the default fragment
+        loadFragment(ExercisesFragment())
+
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_exercises -> loadFragment(ExercisesFragment())
+                R.id.navigation_diet -> loadFragment(DietFragment())
+                R.id.navigation_gym -> loadFragment(GymFragment())
+                R.id.navigation_profile -> loadFragment(ProfileFragment())
+                else -> false
+            }
+            true
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
